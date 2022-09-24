e3b7c8f027ed4589e0a702d89ebbd148a8b95f5e
MazurEgorMaksimovich <egormazur2008@gmail.com>
2022-09-24 11:58:19 +0300

    Add The-Funckciya-7.py

---
+++ The_Funckciya-7.py
@@ -0,0 +1,28 @@
+def plus_minus(s):
+    plus = []
+    minus = []
+    strplus = ''
+    strminus = ''
+    
+    while s != []:
+        if s[0] < 0:
+            minus.append(s[0])
+        else:
+            plus.append(s[0])
+        s = s[1::]
+    
+    plus = sorted(plus)
+    for i in range(0, len(minus)):
+        minus[i] = -minus[i]
+    minus = sorted(minus)
+    for i in range(0, len(minus)):
+        minus[i] = -minus[i]
+
+    for i in plus:
+        strplus = strplus + str(i) + ' '
+    for i in minus:
+        strminus = strminus + str(i) + ' '
+    strvsyo = strplus + '\n' + strminus
+    return strvsyo
+
+print(plus_minus([int(s) for s in input().split()]))
